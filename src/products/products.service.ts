256cf974dd0ee7807a20b2b313ee8a6c0f8fe780
Harry <harrytruong1772@gmail.com>
2022-05-19 23:21:25 +0700

    Initial commit

---
+++ src/products/products.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Product } from "./products.model";
+
+@Injectable()
+export class ProductsService {
+    private products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number) {
+        const prodId = Math.random().toString();
+        const newProduct = new Product(prodId, title, desc, price);
+        this.products.push(newProduct);
+        return prodId;
+    }
+
+    getProducts() {
+        // return this.products; // This will create a door to your data and you can introduce some errors, so basically dont do this!
+        return [...this.products]; // create a copy of array
+    }
+
+    getSingleProduct(productId: string) {
+        const product = this.findProduct(productId)[0];
+        return { ...product };
+    } 
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+        const [product, index] = this.findProduct(productId);
+        const updatedProduct = {...product};
+        if (title) {
+            updatedProduct.title = title;
+        }
+        if (desc) {
+            updatedProduct.desc = desc;
+        }
+        if (price) {
+            updatedProduct.price = price;
+        }
+        this.products[index] = updatedProduct;
+    }
+
+    deleteProduct(prodId: string) {
+        const [_, index] = this.findProduct(prodId);
+        this.products.splice(index, 1);
+    }
+
+    private findProduct(id: string): [Product, number] {
+        const productIndex = this.products.findIndex((prod) => prod.id == id);
+        const product = this.products[productIndex];
+        if (!product) {
+            throw new NotFoundException('Could not find product');
+        }
+        return [product, productIndex];
+    }
+
+    
+}
